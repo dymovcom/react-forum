726c8d491202b918ad959f64da8e3692cdfcb44e
Anton Dymov <dymov.ant@gmail.com>
2023-02-15 00:46:02 +0500

    add errorBoundary

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
+import { ErrorBoundary } from "app/providers/errorBoundary";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { App } from "app";
 import { ThemeProvider } from "app/providers/ThemeProvider";
 import "shared/config/i18n/i18n";
 
-render(<BrowserRouter>
-  <ThemeProvider>
-    <App />
-  </ThemeProvider>
-  {/* eslint-disable-next-line react/jsx-closing-tag-location */}
-</BrowserRouter>, document.getElementById("root"));
+render(
+  (
+    <BrowserRouter>
+      <ErrorBoundary>
+        <ThemeProvider>
+          <App />
+        </ThemeProvider>
+      </ErrorBoundary>
+    </BrowserRouter>
+  ), document.getElementById("root"),
+);
---
+++ src/widgets/pageError/ui/PageError.tsx
@@ -0,0 +1,22 @@
+import { FC } from "react";
+import { useTranslation } from "react-i18next";
+import { Button } from "shared/ui/button";
+import classes from "./PageError.module.scss";
+
+export const PageError: FC = () => {
+  const { t } = useTranslation();
+
+  const reloadPage = () => {
+    // eslint-disable-next-line no-restricted-globals
+    location.reload();
+  };
+
+  return (
+    <div className={classes.PageError}>
+      <p>{t("page_error")}</p>
+      <Button onClick={reloadPage}>
+        {t("page_error_refresh_btn")}
+      </Button>
+    </div>
+  );
+};
